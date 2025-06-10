cb2ce0a94fd707c93f2255e5851821a561df3038
rarelycalmya <chankamya2706@gmail.com>
2025-06-10 09:15:52 +0530

    until 10th june- sliding window median and min

---
+++ Sliding Window/swMedian.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void balance(multiset<long long>&lo, multiset<long long>&hi) {
+        if(hi.size() > lo.size()) {
+            lo.insert(*hi.begin());
+            hi.erase(hi.begin());
+        }
+        if(lo.size() - 1 > hi.size()) {
+            hi.insert(*lo.rbegin());
+            lo.erase(prev(lo.end()));
+        }
+    }
+vector<int> medianSlidingWindow(vector<int>& nums, int k) {
+    multiset<long long> lo, hi;
+    vector<int> ans;
+
+        for(int i = 0; i < (int)nums.size(); i++) {
+            if(lo.empty() || nums[i] <= *lo.rbegin()) lo.insert(nums[i]);
+            else hi.insert(nums[i]);
+            balance(lo, hi);
+
+            if(i >= k) {
+                long long out = nums[i-k];
+                if(out <= *lo.rbegin()) lo.erase(lo.find(out));
+                else hi.erase(hi.find(out));
+                balance(lo, hi);
+            }
+
+            if(i >= k-1) {
+                // if(k % 2 == 1) ans.push_back((double)*lo.rbegin());
+                // else ans.push_back(((double)*lo.rbegin() + (double)*hi.begin() )/2);
+                ans.push_back((int)*lo.rbegin());
+            }
+        }
+    return ans;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> nums;
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+
+    vector<int> ans;
+    ans = medianSlidingWindow(nums, k);
+
+    for(auto i: ans) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
--- Sliding Window/swMin.cpp
+++ Sliding Window/swMin.cpp
@@ -15,10 +15,26 @@ int main() {
         arr.push_back((a*arr[i-1] + b) %c);
     }
 
-    deque<int> q;
-    for(int i = 0; i < k; i++){
-        while(q.size() && arr[q.back() > a[i]])
-    }
+    deque<pair<int, int>> q;
+    long long ans;
+    q.push_back({arr[0], 0});
 
+        for(int i = 1; i < k; i++) {
+            while(!q.empty() && q.back().first > arr[i]) {
+                q.pop_back();
+                // cout << q.front().first << endl;
+            }
+            q.push_back({arr[i], i});   
+        }
+
+        ans = ans ^ q.front().first;
+
+        for(int i = k; i < arr.size(); i++) {
+            while(q.size() && q.back().first > arr[i]) q.pop_back();
+            q.push_back({arr[i], i});
+            if(q.front().second <= i-k) q.pop_front();
+            ans = ans ^ q.front().first;
+        }
+    cout << ans << endl;
     return 0;
 }
